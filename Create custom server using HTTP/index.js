8789a8df5fd3aa155ed6926c24ff615d02a94a65
dudhatmitesh <miteshdudhat5@gmail.com>
2024-10-10 15:19:33 +0530

    first commit

---
+++ Create custom server using HTTP/index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const port = 2222;
+
+const porthandler = (req,res)=>{
+    res.write("<h1 style='color: #333; font-size: 24px; text-align: center;'>Welcome To The Server</h1>");
+    res.end();
+}
+let server = http.createServer(porthandler);
+
+server.listen(port,(err)=>(
+    err ? console.log(err)
+    : console.log(`server Started at http://localhost:${port}`)
+))
